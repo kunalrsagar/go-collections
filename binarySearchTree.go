688c4f736fb3bf4d1c3aa2ee07fdbe108d14a7fc
Kunal Sagar <sagar.kunal@yahoo.com>
2022-05-28 14:37:52 +0530

    removed recursive func from binary search tree

--- binarySearchTree.go
+++ binarySearchTree.go
@@ -10,51 +10,56 @@ func (bst *BinarySearchTree[M]) Insert(value M) {
 		bst.Root = root
 	} else {
 		node := bst.Root
-		CheckNInsert(value, node)
-	}
-}
-
-func CheckNInsert[M Mix](value M, node *TreeNode[M]) {
-	if value > node.Value {
-		// move to right if value > Root.Value
-		if node.Right == nil {
-			// if Right node does not exit, create it
-			newNode := NewTreeNode(value)
-			node.Right = newNode
-		} else {
-			// check the value of node and insert on its left or right
-			CheckNInsert(value, node.Right)
-		}
-	} else {
-		// move to left if value < Root.Value
-		if node.Left == nil {
-			// if Left node does not exit, create it
-			newNode := NewTreeNode(value)
-			node.Left = newNode
-		} else {
-			// check the value of node and insert on its left or right
-			CheckNInsert(value, node.Left)
+		for true {
+			if value > node.Value {
+				// move to right if value > Root.Value
+				if node.Right == nil {
+					// if Right node does not exit, create it
+					newNode := NewTreeNode(value)
+					node.Right = newNode
+					return
+				} else {
+					// check the value of node and insert on its left or right
+					node = node.Right
+					continue
+				}
+			} else {
+				// move to left if value < Root.Value
+				if node.Left == nil {
+					// if Left node does not exit, create it
+					newNode := NewTreeNode(value)
+					node.Left = newNode
+					return
+				} else {
+					// check the value of node and insert on its left or right
+					node = node.Left
+					continue
+				}
+			}
 		}
 	}
 }
 
 func (bst *BinarySearchTree[M]) Lookup(value M) bool {
 	node := bst.Root
-	return checkNodeNValue(value, node)
-}
 
-func checkNodeNValue[M Mix](value M, node *TreeNode[M]) bool {
-	if node != nil {
-		if value > node.Value {
-			// check on right node
-			return checkNodeNValue(value, node.Right)
-		} else if value < node.Value {
-			// check on left node
-			return checkNodeNValue(value, node.Left)
+	for true {
+		if node != nil {
+			if value > node.Value {
+				// check on right node
+				node = node.Right
+				continue
+			} else if value < node.Value {
+				// check on left node
+				node = node.Left
+				continue
+			} else {
+				return true
+			}
 		} else {
-			return true
+			return false
 		}
-	} else {
-		return false
 	}
+
+	return false
 }
